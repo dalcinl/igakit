ab561b547ee8bd07fcd36f3142b4c95bb2cc59dd
Lisandro Dalcin <dalcinl@gmail.com>
2013-09-02 15:06:58 +0300

    Disable f2py messages on array copy

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def setup_package():
                                  'src/igakit/igalib.f90',
                                  ],
                       f2py_options = ['--quiet'],
-                      define_macros = [('F2PY_REPORT_ON_ARRAY_COPY', 0),
+                      define_macros = [#('F2PY_REPORT_ON_ARRAY_COPY', 0),
                                        #('F2PY_REPORT_ATEXIT', 0),
                                        ]
                       ),
